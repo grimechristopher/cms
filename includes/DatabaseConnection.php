2f081ff437d265a87b3c871b9954a427c0cc772a
grimechristopher <chris@chrisgrime.com>
2024-06-28 10:22:08 -0700

    Relearning the quirks of php

--- includes/DatabaseConnection.php
+++ includes/DatabaseConnection.php
@@ -9,7 +9,7 @@ $database_password = $env["DATABASE_PASSWORD"];
 
 // PDO database connection
 try {
-    $pdo = new PDO('mysql:host=$database_host;dbname=cms;charset=utf8', $database_user, $database_password);
+    $pdo = new PDO("mysql:host=$database_host;dbname=cms;charset=utf8", $database_user, $database_password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT, false);
     echo "Connected successfully"; // Optional: Display message on successful connection
