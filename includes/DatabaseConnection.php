5cfcb702375e8cac16bb64dc320891c854495179
grimechristopher <chris@chrisgrime.com>
2024-06-28 09:44:25 -0700

    adjustments to database location

--- includes/DatabaseConnection.php
+++ includes/DatabaseConnection.php
@@ -1,14 +1,15 @@
 <?php
 // Load environment variables from .env file
-$env = parse_ini_file('.env');
+$env = parse_ini_file('/../.env'); // root of project
 
 // Set database credentials from .env variables
+$database_host = $env["DATABASE_HOST"];
 $database_user = $env["DATABASE_USER"];
 $database_password = $env["DATABASE_PASSWORD"];
 
 // PDO database connection
 try {
-    $pdo = new PDO('mysql:host=localhost;dbname=cms;charset=utf8', $database_user, $database_password);
+    $pdo = new PDO('mysql:host=$database_host;dbname=cms;charset=utf8', $database_user, $database_password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully"; // Optional: Display message on successful connection
 } catch (PDOException $e) {
