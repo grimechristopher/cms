3f98a8afe2a9a7d464bac71455444de58ffec493
grimechristopher <chris@chrisgrime.com>
2024-06-27 20:10:45 -0700

    add gitignore

--- includes/DatabaseConnection.php
+++ includes/DatabaseConnection.php
@@ -1,3 +1,3 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;dbname=cms;charset=utf8', '_username_', '_password_');
+$pdo = new PDO('mysql:host=localhost;dbname=cms;charset=utf8', 'root', 'pgGrime06');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
