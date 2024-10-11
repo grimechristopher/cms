40753af6c6b5ed0084966d1ad516001c0ccb2ccb
grimechristopher <chris@chrisgrime.com>
2024-10-11 15:56:53 -0700

    update auth to work with jwt auth

--- classes/Cms/Controllers/Login.php
+++ classes/Cms/Controllers/Login.php
@@ -2,52 +2,54 @@
 namespace Cms\Controllers;
 
 class Login {
-	private $authentication;
+    private $authentication;
 
-	public function __construct(\Ninja\Authentication $authentication) {
-		$this->authentication = $authentication;
-	}
+    public function __construct(\Ninja\Authentication $authentication) {
+        $this->authentication = $authentication;
+    }
 
-	public function loginForm() {
-		return ['template' => 'login.html.php', 'title' => 'Log In'];
-	}
+    public function loginForm() {
+        $supabaseAuthUrl = 'https://your-supabase-url.supabase.co/auth/v1/authorize?provider=email';
+        header('Location: ' . $supabaseAuthUrl);
+        exit();
+    }
 
-	public function processLogin() {
-		if ($this->authentication->login($_POST['email'], $_POST['password'])) {
+    public function processLogin() {
+        if ($this->authentication->login($_POST['email'], $_POST['password'])) {
             header('Location: index.php?login/success'); 
-		}
-		else {
-			return ['template' => 'login.html.php',
-					'title' => 'Log In',
-					'variables' => [
-						'error' => 'Invalid username/password.'
-						]
-					];
-		}
-	}
+        }
+        else {
+            return ['template' => 'login.html.php',
+                    'title' => 'Log In',
+                    'variables' => [
+                        'error' => 'Invalid username/password.'
+                        ]
+                    ];
+        }
+    }
 
-	public function success() {
-		$title = 'Course Timetabling';
-		$user = $this->authentication->getUser();
-		return ['template' => 'home.html.php',
-				'title' => $title, 
-				'variables' => [
-					'user' => $user
-					]
-				];
-	}
+    public function success() {
+        $title = 'Course Timetabling';
+        $user = $this->authentication->getUser();
+        return ['template' => 'home.html.php',
+                'title' => $title, 
+                'variables' => [
+                    'user' => $user
+                    ]
+                ];
+    }
 
-	public function error() {
-		return ['template' => 'loginerror.html.php', 'title' => 'You are not logged in'];
-	}
+    public function error() {
+        return ['template' => 'loginerror.html.php', 'title' => 'You are not logged in'];
+    }
 
-	public function permissionsError() {
-		return ['template' => 'permissionserror.html.php', 'title' => 'Access Denied'];
-	}
+    public function permissionsError() {
+        return ['template' => 'permissionserror.html.php', 'title' => 'Access Denied'];
+    }
 
-	public function logout() {
-		unset($_SESSION);
-		session_destroy();
-		return ['template' => 'logout.html.php', 'title' => 'You have been logged out'];
-	}
+    public function logout() {
+        unset($_SESSION);
+        session_destroy();
+        return ['template' => 'logout.html.php', 'title' => 'You have been logged out'];
+    }
 }
